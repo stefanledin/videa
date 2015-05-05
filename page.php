b334af3f4285984699bdaebb8880d63687ebe170
stefanledin <stefanledin@gmail.com>
2015-05-05 07:02:05 +0200

    page.php

---
+++ page.php
@@ -0,0 +1,18 @@
+<?php get_header(); ?>
+	<?php if ( have_posts() ) : while ( have_posts() ) : the_post(); ?>
+		<section class="site-width">
+			<div class="site-width-inner">
+				<?php the_post_thumbnail( ); ?>
+			</div>
+		</section>
+		<section class="site-width grid-660">
+			<div class="site-with-inner">
+				<article>
+					<h1 class="title serif"><?php the_title();?></h1>
+					<?php the_content(); ?>
+					<a class="home-link" href="<?php echo home_url();?>">« Tillbaka</a>
+				</article>
+			</div>
+		</section>
+	<?php endwhile; endif; ?>
+<?php get_footer(); ?>
