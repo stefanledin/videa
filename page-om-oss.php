1d515dfe277155938b62101372be17b51277f823
stefanledin <stefan.ledin@gmail.com>
2015-06-01 09:55:39 +0200

    justerat typo

--- page-om-oss.php
+++ page-om-oss.php
@@ -1,15 +1,21 @@
 <?php get_header(); ?>
-
+	<?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
+	<section class="site-width">
+		<div class="site-width-inner">
+			<?php the_post_thumbnail( ); ?>
+		</div>
+	</section>
 	<section class="site-width grid-660">
 		<div class="site-width-inner">
 			<article class="about-us">
-				<?php if ( have_posts() ) : while( have_posts() ) : the_post(); ?>
-					<h3 class="section-title"><?php the_title();?></h3>
+					<header class="page-title">
+						<h1 class="title serif"><?php the_title();?></h1>
+					</header>
 					<?php the_content(); ?>
-				<?php endwhile; endif; ?>
 			</article>
 		</div>
 	</section>
+	<?php endwhile; endif; ?>
 	<section class="site-width">
 		<div class="site-width-inner">
 			<div class="contact-list clearfix">
